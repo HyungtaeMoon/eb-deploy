fd2cb3fff0561d73b0f77dadaff948b03f31277f
HyungtaeMoon <blessmht@gmail.com>
2018-09-15 19:56:05 +0900

    production에서 ALLOWED_HOSTS 비밀값으로 설정

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -1,10 +1,9 @@
 from .base import *
 
-DEBUG = True
+secrets = json.load(open(os.path.join(SECRETS_DIR, 'production.json')))
 
-ALLOWED_HOSTS = [
-    'localhost',
-]
+DEBUG = False
+ALLOWED_HOSTS = secrets['ALLOWED_HOSTS']
 
 WSGI_APPLICATION = 'config.wsgi.production.application'
 
@@ -23,3 +22,44 @@ DATABASES = {
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOG_DIR = '/var/log/django'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'django.server': {
+            'format': '[%(asctime)s] %(message)s',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'file_error': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'ERROR',
+            'formatter': 'django.server',
+            'backupCount': 10,
+            'filename': os.path.join(LOG_DIR, 'error.log'),
+            'maxBytes': 10485760,
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file_error'],
+            'level': 'INFO',
+            'propagate': True,
+        }
+    }
+}
