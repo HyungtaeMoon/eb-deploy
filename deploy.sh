e430d324ce4963579e22711265df6a75e5f29ba5
HyungtaeMoon <blessmht@gmail.com>
2018-09-21 20:05:43 +0900

    http요청시 https로 redirect설정

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+git add -A
 git add -f .secrets/
 
 # eb deploy 실행
@@ -6,6 +7,4 @@ eb deploy --profile sub-baeminchan-django-eb --staged
 
 # .secrets와 requirements를 staging area엑서 제거
 git reset HEAD .secrets/ requirements.txt
-
-# requirements.txt 삭제
-rm requirements.txt
+git reset
